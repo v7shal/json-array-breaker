5c372c97f001b43184b88e71bcfcf0b64a66892e
v7shal <vishal250897sharma@gmail.com>
2024-09-19 10:50:52 -0700

    this is my first commit to JAB
    JAB - Json Array Breaker

---
+++ src/main/java/vs/project/json_array/JsonArrayApplication.java
@@ -0,0 +1,13 @@
+package vs.project.json_array;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonArrayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonArrayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vs/project/json_array/JsonArrayApplicationTests.java
@@ -0,0 +1,13 @@
+package vs.project.json_array;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonArrayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
